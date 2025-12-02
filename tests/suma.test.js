a69c38db7d6ddffef76adefcd48975b7158e0fc6
Lucciana Vergara Martinez <lucianavergaramartinez@gmail.com>
2025-12-02 14:50:16 -0500

    style: Apply prettier formatting

--- tests/suma.test.js
+++ tests/suma.test.js
@@ -1,6 +1,6 @@
-const suma = require("../app");
+const suma = require('../src/suma.js');
 
-test("La función suma debe sumar correctamente", () => {
+test('La función suma debe sumar correctamente', () => {
   expect(suma(2, 3)).toBe(5);
   expect(suma(-1, 1)).toBe(0);
   expect(suma(10, 20)).toBe(30);
