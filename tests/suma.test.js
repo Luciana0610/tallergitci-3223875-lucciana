21342d97a5ef2d1543e870d90ea30c391b4eed15
Lucciana Vergara Martinez <lucianavergaramartinez@gmail.com>
2025-12-02 08:06:21 -0500

    feat: estructura inicial del proyecto

---
+++ tests/suma.test.js
@@ -0,0 +1,7 @@
+const suma = require("../app");
+
+test("La función suma debe sumar correctamente", () => {
+  expect(suma(2, 3)).toBe(5);
+  expect(suma(-1, 1)).toBe(0);
+  expect(suma(10, 20)).toBe(30);
+});
